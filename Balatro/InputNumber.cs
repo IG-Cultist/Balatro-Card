b379d57b2046672e5d76b9e03f6efb4c13dec510
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-06-21 10:24:32 +0900

    入力された4つの値を確認し、以下の判定を行う #2 判定結果を出力する #3

--- Balatro/InputNumber.cs
+++ Balatro/InputNumber.cs
@@ -21,7 +21,7 @@ namespace Balatro
                 while (loop == true)
                 {
                     //Console.Clear();
-                    Console.Write("1枚目の数値[1,2,3,4]:");
+                    Console.Write((i + 1) + "枚目の数値[1,2,3,4]:");
                     numStr = Console.ReadLine();
                     //数値以外または範囲外の文字列を入力した際、再入力
                     if (int.TryParse(numStr, out numbers[i])
@@ -33,6 +33,32 @@ namespace Balatro
                     }
                 }
                 number[i] = numbers[i];
+            }
+
+            int count = 0;
+            for (int i = 0; i < number.Count() - 1; i++)
+            {
+                for (int j = i + 1; j < number.Count(); j++)
+                {
+                    if ((number[i] & 0xF) == (number[j] & 0xF))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            // 成立した役を返す
+            switch (count)
+            {
+                case 1: return 1;
+                case 2: return 2;
+                case 3: return 3;
+            }
+
+            //フォーカード
+            if ((number[0] == number[1]) && (number[0] == number[2]) && (number[0] == number[3]))
+            {
+                result = 4;
             }
 
             return result;
--- Balatro/Program.cs
+++ Balatro/Program.cs
@@ -12,10 +12,35 @@ namespace Balatro
         static void Main(string[] args)
         {
             int[] number = new int[4];
+            int result = 0;
             InputNumber inputNumber = new InputNumber();
-            inputNumber.InputNum(number);
-        }
+            result = inputNumber.InputNum(number);
 
-       
+            if(result == 1)
+            {
+                Console.WriteLine("ワンペア");
+                Console.ReadLine();
+            }
+            else if (result == 2)
+            {
+                Console.WriteLine("ツーペア");
+                Console.ReadLine();
+            }
+            else if (result == 3)
+            {
+                Console.WriteLine("スリーカード");
+                Console.ReadLine();
+            }
+            else if (result == 4 )
+            {
+                Console.WriteLine("フォーカード");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("ハンド無し！");
+                Console.ReadLine();
+            }
+        }
     }
 }
